59973490df3357c7d5d1dbfe2e1ee5b402634454
topdev0528 <top.dev0528@gmail.com>
2022-06-02 04:09:26 -0400

    Add: signUp api

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -5,6 +5,7 @@ const userModel = require("../models/users.js");
 const { uuid } = require("uuidv4");
 
 var jwt = require("jsonwebtoken");
+const { JWT_SECRET_KEY, JWT_TOKEN_EXPIRATION } = process.env;
 
 module.exports = {
   async logIn(req, res) {
@@ -14,6 +15,10 @@ module.exports = {
     }
 
     const { phone } = req.body;
+    res.json({
+      status: status.OK,
+      message: "PIN code is sent.",
+    });
   },
 
   async verifyOPT(req, res) {
@@ -21,10 +26,37 @@ module.exports = {
       res.status(status.BAD_REQUEST).json();
       return;
     }
-    res.json({
-      user_exist: false,
-      status: true,
-    });
+
+    const { phone } = req.body;
+    const user = await userModel.getUser(phone);
+    console.log(user);
+
+    if (user?.user_id) {
+      const token = jwt.sign(
+        {
+          uid: user.user_id,
+          userData: {
+            userId: user.user_id,
+            walletAddress: user.wallet_address,
+          },
+        },
+        JWT_SECRET_KEY,
+        {
+          expiresIn: JWT_TOKEN_EXPIRATION,
+        }
+      );
+      res.json({
+        user_exist: true,
+        status: status.OK,
+        token: token,
+        user: user,
+      });
+    } else {
+      res.json({
+        user_exist: false,
+        status: status.OK,
+      });
+    }
   },
 
   async signUp(req, res) {
@@ -36,8 +68,32 @@ module.exports = {
     const userId = uuid();
     const { phone, wallet_address } = req.body;
     const user = await userModel.create(userId, phone, wallet_address);
-    res.json({
-      user: user,
-    });
+
+    if (user?.user_id) {
+      const token = jwt.sign(
+        {
+          uid: user.user_id,
+          userData: {
+            userId: user.user_id,
+            walletAddress: user.wallet_address,
+          },
+        },
+        JWT_SECRET_KEY,
+        {
+          expiresIn: JWT_TOKEN_EXPIRATION,
+        }
+      );
+      res.json({
+        user_exist: true,
+        status: status.OK,
+        token: token,
+        user: user,
+      });
+    } else {
+      res.json({
+        user_exist: false,
+        status: status.OK,
+      });
+    }
   },
 };
