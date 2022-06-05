2773f3968b87c2d6fd4e46f04d25c47c100995c9
topdev0528 <top.dev0528@gmail.com>
2022-06-05 14:57:01 -0400

    Add: redeemEvent api

--- src/controllers/events.js
+++ src/controllers/events.js
@@ -85,12 +85,7 @@ module.exports = {
       return;
     }
 
-    if (!has(req.body, ["user_id"])) {
-      res.status(status.BAD_REQUEST).json();
-      return;
-    }
-
-    const { user_id, event_id } = req.body;
+    const { event_id } = req.body;
 
     let events = await eventModel.redeemEvent(event_id);
     let event = events[0];
